6a3140b41f7eadccde8a2e3cc908babb4cd1b9ef
Josh Kuhn <deontologician@gmail.com>
2022-05-20 15:36:14 -0700

    Implement EKS

--- cdk/lib/app-stack.ts
+++ cdk/lib/app-stack.ts
@@ -1,20 +1,32 @@
-import { Stack, StackProps, aws_ecs_patterns as ecsPatterns, aws_ecs as ecs } from "aws-cdk-lib";
+import { Stack, StackProps, aws_eks as eks, aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const loadBalancedEcsService = new ecsPatterns.ApplicationLoadBalancedEc2Service(this, 'Service', {
-      memoryLimitMiB: 1024,
-      taskImageOptions: {
-        image: ecs.ContainerImage.fromAsset(`${__dirname}/../../app`, {
-        }),
-        environment: {
-          "FOO": "BAR",
-        },
-      },
-      desiredCount: 3,
+    const vpc = new ec2.Vpc(this, "VPC");
+
+    const cluster = new eks.Cluster(this, "Cluster", {
+      vpc,
+      version: eks.KubernetesVersion.V1_21,
+      defaultCapacity: 0,
     });
+
+    cluster.addNodegroupCapacity("gpu-nodes", {
+      instanceTypes: [
+        ec2.InstanceType.of(ec2.InstanceClass.P2, ec2.InstanceSize.XLARGE),
+      ],
+      minSize: 0,
+      maxSize: 1,
+      capacityType: eks.CapacityType.SPOT,
+      taints: [
+        {
+          effect: eks.TaintEffect.NO_SCHEDULE,
+          key: "requires-gpu",
+          value: "true",
+        }
+      ]
+    })
   }
 }

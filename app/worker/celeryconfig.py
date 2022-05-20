d6eaa6769abf9b71387e60ebf87bd0017706134a
Josh Wilson <person142@users.noreply.github.com>
2022-05-20 16:30:04 -0700

    Add script that randomly restarts the worker while hitting the server
    Also set acks late to true in celery, which should make it handle
    crashes. Currently the script reports that we still get failures when
    the worker goes down; haven't figured out why yet.

--- app/worker/celeryconfig.py
+++ app/worker/celeryconfig.py
@@ -3,3 +3,6 @@ result_backend = "redis://redis:6379/0"
 
 # We load the model at worker start; give it plenty of time.
 worker_proc_alive_timeout = 60
+
+# If the spot instance goes down, we want to retry the tasks.
+task_acks_late = True
